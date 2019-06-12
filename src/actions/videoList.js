e6b4d304286a2fd088fc7ceabcf622c5511df5e9
dlwong <pingki@sbcglobal.net>
2019-06-12 15:53:31 -0700

    Pass videoList specs

--- src/actions/videoList.js
+++ src/actions/videoList.js
@@ -1,5 +1,9 @@
+import exampleVideoData from '../data/exampleVideoData.js';
+
 var changeVideoList = (videos) => ({
   //TODO:  Return some action object to change the list of videos in VideoList.
+  type: 'CHANGE_VIDEO_LIST',
+  videos: exampleVideoData
 });
 
 export default changeVideoList;
