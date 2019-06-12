f3f3d6aa2269db9f0001945ab5bb5f348cc10c53
dlwong <pingki@sbcglobal.net>
2019-06-12 15:42:54 -0700

    Pass changeVideo specs

--- src/actions/currentVideo.js
+++ src/actions/currentVideo.js
@@ -1,5 +1,9 @@
+import exampleVideoData from '../data/exampleVideoData.js';
+
 var changeVideo = (video) => ({
   //TODO:  Return some action object to change the currently playing video.
+  type: 'CHANGE_VIDEO',
+  video: exampleVideoData[0]
 });
 
 export default changeVideo;
